816776e7ac5cdf1110efd4332c7e909527bc44dd
Christian Nagel <christian@christiannagel.com>
2017-09-12 15:37:59 +0200

    collections

---
+++ Generics/CollectionSample/CollectionSample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ListDemo();
+            // QueueDemo();
+            StackDemo();
+        }
+
+        private static void StackDemo()
+        {
+            Stack<int> myStack = new Stack<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                myStack.Push(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int n = myStack.Pop();
+                Console.WriteLine($"retrieved {n}");
+            }
+        }
+
+        private static void QueueDemo()
+        {
+
+            Queue<int> myQueue = new Queue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int n = myQueue.Dequeue();
+                Console.WriteLine($"retrieved {n}");
+            }
+        }
+
+        private static void ListDemo()
+        {
+            List<int> intList = new List<int>(2200);
+            for (int i = 0; i < 100; i++)
+            {
+                intList.Add(i);
+                Console.WriteLine($"added {i}, capacity: {intList.Capacity}");
+            }
+
+        }
+    }
+}
--- lab04.md
+++ lab04.md
@@ -10,3 +10,13 @@
 1. Define the Accumulate method using generic parameters and a generic delegate that's used within the method
 2. Use the Accumulate method with int and string
 
+## Collection Classes
+
+1. Use List<T>, fill the collection and iterate through it
+
+> What is the capacity of List<T>?
+
+2. Use Stack<T> and Queue<T>
+3. Use LinkedList<T>
+4. Use Dictionary<TKey, TValue>
+
