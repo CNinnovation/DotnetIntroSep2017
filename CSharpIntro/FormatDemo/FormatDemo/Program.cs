c69000174ccff69f3c3c1f36710ef1aaeb2f1684
Christian Nagel <christian@christiannagel.com>
2017-09-13 10:33:04 +0200

    IFormattable

---
+++ CSharpIntro/FormatDemo/FormatDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 42;
+            Console.WriteLine($"{n:X} {n:e}");
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"{today:D} {today:d}");
+        }
+    }
+}
